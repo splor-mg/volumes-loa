035adc6f1ed592d0a7df961d6644d47a381a12a9
Andrey Morais Labanca <moraislabanca@gmail.com>
2023-09-13 17:13:08 -0300

    commit saindo da camg, se deus quiser pro branch certo.

--- utils/transfom-data.py
+++ utils/transfom-data.py
@@ -2,30 +2,39 @@ import ntpath
 
 import pandas as pd
 from pathlib import Path
-from glob import glob
+from frictionless import Package, Resource
 
 df_dict = {}
-map_bancos = {}
+bancos_names = ["BASE_CATEGORIA_PESSOAL",
+              "BASE_DETALHAMENTO_OBRAS",
+              "BASE_ORCAM_DESPESA_ITEM_FISCAL",
+              "BASE_ORCAM_RECEITA_FISCAL",
+              "BASE_QDD_FISCAL",
+              "BASE_QDD_INVESTIMENTO",
+              "BASE_QDD_PLURIANUAL_INVEST",
+              "BASE_REPASSE_RECURSOS",
+            ]
+datapackage_path = Path.joinpath(Path(__file__).parents[1], 'datapackages/sisor-dados-2024')
+datapackage_dataraw_path = Path.joinpath(Path(__file__).parents[1], 'datapackages/sisor-dados-2024/data-raw/')
+bancos_sisor_path = Path.joinpath(Path(__file__).parents[1], 'bancos/SISOR/')
 
+data_filenames = [Path.joinpath(datapackage_dataraw_path, y + '.html') for x in ([datapackage_dataraw_path]) for y in map(str.lower, bancos_names)]
 
-data_path = Path.joinpath(Path(__file__).parents[1], 'bancos/SISOR/*.xlsx').as_posix()
-print(data_path)
+#source_descriptor = Path.joinpath(datapackage_path , 'datapackage.json')
+#resource_name = bancos_names[0].lower()
+#package = Package(source_descriptor)
+#resource = package.get_resource(resource_name)
+#resource.transform(transform_pipeline)
+#table = resource.to_petl()
+#for field in resource.schema.fields:
+#    if field.custom.get('target'):
+#        table = etl.rename(table, field.name, field.custom['target'])
 
-bancos_filenames = [Path(x).as_posix() for x in (glob(data_path))]
-
-
-for filename in bancos_filenames:
+for filename in data_filenames:
     print(filename)
+    df = pd.read_html(filename, header=0, index_col=0, decimal=',', thousands='.')
+    save_path = Path.joinpath(bancos_sisor_path, filename.stem + '.xlsx' )
+    df[0].to_excel(save_path, sheet_name=filename.stem.upper())
 
-datapackage_path = Path.joinpath(Path(__file__).parents[1], 'datapackages/sisor-dados-2024/data/*.csv').as_posix()
-datapackage_filenames = [Path(x).as_posix() for x in (glob(datapackage_path))]
-print(datapackage_filenames)
-
-for filename in datapackage_filenames:
-    print(filename)
-
-for filename in datapackage_filenames:
-    df =  pd.read_csv(filename)
-    df_dict[Path(str.upper(filename)).stem] = df
-
-print(df_dict)
+# install-in.txt -> piptools compilte -> gera requirements.txt ->pip install requirements.txt (ver pacotes que ele não achou por não exisitrem para pthon 3.7 e colocar a ultima versão disponivel no requirements-in.txt
+# os requirements-in fica no repo volumes-docker
