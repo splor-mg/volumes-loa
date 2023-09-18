3af5f543375e091ddc164f3fe569465d0c545d12
labanca <moraislabanca@gmail.com>
2023-09-18 12:41:25 -0300

    Utiliza dpm install para obtenção dos bancos (#67)
    * commit para camg
    
    * commit saindo da camg, se deus quiser pro branch certo.
    
    * solves SISOR download via DPM install - issue 41
    
    * read_html_table.py le datapackages sisor e converte tabelas html para xlsx.
    
    * read_txt_sigplan.py fluxo de arquivos do sigplan para uso no volumes-loa
    
    * le arquivo txt em read_txt_sigplan.py em utf-8 devido a conversão já ser feita por encode_utf8.sh
    
    * utiliza frictionless para importacao html e exportacao xslx
    
    * execucao make rm vol=4 && make v4
    
    * remove arquivos desnecessarios
    
    * adapta utils/read_txt_sigplan.py para exclusão de utils/encode_utf8.sh do mark format
    
    see splor-mg/volumes-loa#68
    
    * dpm install && make format
    
    ---------
    
    Co-authored-by: Francisco Júnior <fjunior.alves.oliveira@gmail.com>

---
+++ utils/read_html_sisor.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from frictionless import Package, formats
+
+package = Package('datapackages/sisor/datapackage.json')
+
+resource_names = [
+                  "base_categoria_pessoal",
+                  "base_detalhamento_obras",
+                  "base_orcam_despesa_item_fiscal",
+                  "base_orcam_receita_fiscal",
+                  "base_qdd_fiscal",
+                  "base_qdd_investimento",
+                  "base_qdd_plurianual_invest",
+                  "base_repasse_recursos",
+                 ]
+
+for resource_name in resource_names:
+  print("Formata recurso", resource_name)
+  resource = package.get_resource(resource_name)
+  excel_control = formats.ExcelControl(sheet=resource_name.upper())
+  output_path = str(Path('bancos/SISOR/') / f'{resource_name.upper()}.xlsx')
+  resource.write(output_path, control = excel_control)
+  print(f"Arquivo salvo em {output_path}")
