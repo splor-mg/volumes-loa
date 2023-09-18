afee90c17af7dee08c36ec717df1f77efb620937
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-09-18 15:12:16 -0300

    remove cat(.libPaths()) do pdf volume 5 (#59)

--- volume5/Rnw/CodigosR/ANEXOS.R
+++ volume5/Rnw/CodigosR/ANEXOS.R
@@ -3,7 +3,6 @@
   cat("\\footnotesize\n") 
  
   cat("\\renewcommand*{\\arraystretch}{2}\n") 
-  cat(.libPaths()) 
   grupo_despesa = data.table(read_excel(paste0(dir_bancos, "/manual/desc_grupos_de_despesa.xlsx"), sheet=1))[order(CODIGO)] 
   fontes = data.table(read_excel(paste0(dir_bancos, "/manual/desc_fontes_de_recursos.xlsx"), sheet=1))[order(CODIGO)] 
   iag = data.table(read_excel(paste0(dir_bancos, "/manual/desc_IAG.xlsx"), sheet=1))[order(CODIGO)] 
