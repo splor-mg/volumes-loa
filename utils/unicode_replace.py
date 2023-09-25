be909bc5a9d18a6131c6d49a72b27620f7d07576
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-09-25 08:30:10 -0300

    altera log de notificacao utils/unicode_replace.py

--- utils/unicode_replace.py
+++ utils/unicode_replace.py
@@ -1,34 +1,86 @@
 import pandas as pd
 import numpy as np
+import unicodedata
+import typer
+from typing_extensions import Annotated
+import textwrap
 
-df = pd.read_csv('utils/invalids_chars.csv', header=0)
-char_to_find = df.values
-char_to_find = np.vectorize(lambda x: int(x, 16))(char_to_find)
+def format_integer_list(integers):
+    formatted_ranges = []
+    
+    if not integers:
+        return ""
 
-filenames = [
-    'bancos/SISOR/acoes_planejamento.txt',
-]
-for file_path in filenames:
+    start = end = integers[0]
 
-    print(f"Buscando caracters inválidos no arquivo: {file_path}")
-    for row in char_to_find:
+    for i in range(1, len(integers)):
+        if integers[i] == end + 1:
+            end = integers[i]
+        else:
+            if start == end:
+                formatted_ranges.append(str(start))
+            else:
+                formatted_ranges.append(f"{start}-{end}")
+            start = end = integers[i]
 
-        try:
-            with open(file_path, 'rb') as file:
-                data = file.read()
+    if start == end:
+        formatted_ranges.append(str(start))
+    else:
+        formatted_ranges.append(f"{start}-{end}")
 
-            # Replace the character
-            modified_data = data.replace(chr(row[0]).encode('utf-8'), chr(row[1]).encode('utf-8'))
+    return ", ".join(formatted_ranges)
 
-            # Write the modified data back to the file
+def main(dry_run: Annotated[bool, typer.Option(help="Show which characters would be replaced without actually replacing them.")] = False):
+    df = pd.read_csv('utils/invalids_chars.csv', header=0)
+    char_to_find = df.values
+    char_to_find = np.vectorize(lambda x: int(x, 16))(char_to_find)
+
+    filenames = [
+        'bancos/SISOR/acoes_planejamento.txt',
+    ]
+
+    for file_path in filenames:
+        print(f"Buscando caracteres inválidos no arquivo: {file_path}")
+        
+        # Create a dictionary to store character replacements and line numbers
+        replacement_dict = {}
+
+        with open(file_path, 'rb') as file:
+            modified_lines = []  # A list to store modified lines
+
+            for line_number, line in enumerate(file, start=1):
+                modified_line = line  # Initialize modified_line with the original line
+
+                for row in char_to_find:
+                    # Replace the character in the line
+                    modified_line = modified_line.replace(chr(row[0]).encode('utf-8'), chr(row[1]).encode('utf-8'))
+                    
+                    # Check if the line was modified and record the line number
+                    if line != modified_line:
+                        from_char = f"{hex(row[0])}"
+                        to_char = f"{hex(row[1])}"
+                        from_url = f"https://symbl.cc/en/{from_char[2:].zfill(4)}/"
+                        to_url = f"https://symbl.cc/en/{to_char[2:].zfill(4)}/"
+                        unicode_pair_info = textwrap.dedent(f"""
+                                             De: {from_char} ({unicodedata.name(chr(row[0]), 'Unknown')}) - {from_url}
+                                             Para: {to_char} ({unicodedata.name(chr(row[1]), 'Unknown')}) - {to_url}
+                                             """)
+                        if unicode_pair_info not in replacement_dict:
+                            replacement_dict[unicode_pair_info] = [line_number]
+                        else:
+                            replacement_dict[unicode_pair_info].append(line_number)
+
+                # Append the modified line to the list
+                modified_lines.append(modified_line)
+
+        # Print character replacements and corresponding line numbers
+        for unicode_pair_info, line_numbers in replacement_dict.items():
+            formatted_line_numbers = format_integer_list(line_numbers)
+            print(f"{unicode_pair_info}Linhas: {formatted_line_numbers}\n")
+
+        if not dry_run:
             with open(file_path, 'wb') as file:
-                file.write(modified_data)
+                file.writelines(modified_lines)
 
-            if data != modified_data:
-                print(f"Caractere unicode {row[0]} substituído pelo unicode {row[1]} no arquivo {file_path}.")
-
-        except FileNotFoundError:
-            print(f"File not found: {file_path}")
-
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
+if __name__ == "__main__":
+    typer.run(main)
