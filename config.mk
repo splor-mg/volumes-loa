cf827c029b3a53cb4f8d05a1ecdcbf379921d08f
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-09-26 16:46:09 -0300

    touch .env in config.mk (#78)

--- config.mk
+++ config.mk
@@ -12,6 +12,8 @@ DEPENDENCIAS_V5 := $(shell Rscript --verbose utils/makefile/gera_dependencias_v5
 
 acoes_planejamento := $(shell Rscript --verbose utils/makefile/ultimo_banco_mod.R bancos/SISOR/acoes_planejamento 2> logs/log.Rout)
 
+# .env must exist otherwise we get ".env: No such file or directory" error
+touch .env 
 include .env
 
 # variaveis utilizadas pelo target make docker
