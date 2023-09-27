6d5249f3bbaf6011df66f250da7d843640a1131b
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-09-27 11:49:46 -0300

    insert Rscript back into call (#78)

--- config.mk
+++ config.mk
@@ -4,17 +4,17 @@ include .env
 
 #====================================================================
 # Gera dependências para os volumes
-DEP_PRODEMGE_PDF_V1 := $(shell $(VERBOSE) utils/makefile/gera_dep_PRODEMGE_PDF_v1.R 2> logs/log.Rout)
-DEP_DCGF_PDF_V1 := $(shell $(VERBOSE) utils/makefile/gera_dep_DCGF_PDF_v1.R 2> logs/log.Rout)
-DEP_QDD_FISCAL_TXT_V1 := $(shell $(VERBOSE) utils/makefile/gera_dep_QDD_FISCAL_TXT_v1.R 2> logs/log.Rout)
-DEP_RECEITA_TXT_V1 := $(shell $(VERBOSE) utils/makefile/gera_dep_RECEITA_TXT_v1.R 2> logs/log.Rout)
-DEP_REC_QDD_TXT_V1 := $(shell $(VERBOSE) utils/makefile/gera_dep_RECEITA-QDD_FISCAL_TXT_v1.R 2> logs/log.Rout)
-DEPENDENCIAS_V2 := $(shell $(VERBOSE) utils/makefile/gera_dependencias_v2.R 2> logs/log.Rout)
-DEPENDENCIAS_V3 := $(shell $(VERBOSE) utils/makefile/gera_dependencias_v3.R 2> logs/log.Rout)
-DEPENDENCIAS_V4 := $(shell $(VERBOSE) utils/makefile/gera_dependencias_v4.R 2> logs/log.Rout)
-DEPENDENCIAS_V5 := $(shell $(VERBOSE) utils/makefile/gera_dependencias_v5.R 2> logs/log.Rout)
+DEP_PRODEMGE_PDF_V1 := $(shell Rscript $(VERBOSE) utils/makefile/gera_dep_PRODEMGE_PDF_v1.R 2> logs/log.Rout)
+DEP_DCGF_PDF_V1 := $(shell Rscript $(VERBOSE) utils/makefile/gera_dep_DCGF_PDF_v1.R 2> logs/log.Rout)
+DEP_QDD_FISCAL_TXT_V1 := $(shell Rscript $(VERBOSE) utils/makefile/gera_dep_QDD_FISCAL_TXT_v1.R 2> logs/log.Rout)
+DEP_RECEITA_TXT_V1 := $(shell Rscript $(VERBOSE) utils/makefile/gera_dep_RECEITA_TXT_v1.R 2> logs/log.Rout)
+DEP_REC_QDD_TXT_V1 := $(shell Rscript $(VERBOSE) utils/makefile/gera_dep_RECEITA-QDD_FISCAL_TXT_v1.R 2> logs/log.Rout)
+DEPENDENCIAS_V2 := $(shell Rscript $(VERBOSE) utils/makefile/gera_dependencias_v2.R 2> logs/log.Rout)
+DEPENDENCIAS_V3 := $(shell Rscript $(VERBOSE) utils/makefile/gera_dependencias_v3.R 2> logs/log.Rout)
+DEPENDENCIAS_V4 := $(shell Rscript $(VERBOSE) utils/makefile/gera_dependencias_v4.R 2> logs/log.Rout)
+DEPENDENCIAS_V5 := $(shell Rscript $(VERBOSE) utils/makefile/gera_dependencias_v5.R 2> logs/log.Rout)
 
-acoes_planejamento := $(shell $(VERBOSE) utils/makefile/ultimo_banco_mod.R bancos/SISOR/acoes_planejamento 2> logs/log.Rout)
+acoes_planejamento := $(shell Rscript $(VERBOSE) utils/makefile/ultimo_banco_mod.R bancos/SISOR/acoes_planejamento 2> logs/log.Rout)
 
 # variaveis utilizadas pelo target make docker
 # vide https://github.com/splor-mg/volumes-loa/issues/24
