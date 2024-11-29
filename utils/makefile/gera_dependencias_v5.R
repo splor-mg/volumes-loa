4d00827f83dff2609a65a0e4bdb5dd7419737ac6
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-11-29 11:50:45 -0300

    Corrige script chama capaLOA geracao vol5
    See #159
    See #160

--- utils/makefile/gera_dependencias_v5.R
+++ utils/makefile/gera_dependencias_v5.R
@@ -2,11 +2,11 @@
 # Gera todas as dependências do volume 5
 
 dependencias = c("volume5/data/*.txt",
-                 "volume5/Rnw/ANEXOS.Rnw", 
-                 "volume5/Rnw/capaLOA.pdf",
-                 "volume5/Rnw/demonstrativo_consolidado_despesav1.Rnw", 
+                 "volume5/Rnw/ANEXOS.Rnw",
+                 "volume5/Rnw/CapaLOA.pdf",
+                 "volume5/Rnw/demonstrativo_consolidado_despesav1.Rnw",
                  "volume5/Rnw/load_bibliotecas.tex",
-                 "volume5/Rnw/Projeto_volume5.Rnw", 
+                 "volume5/Rnw/Projeto_volume5.Rnw",
                  "volume5/Rnw/QUADRO_DETALHAMENTO_DESPESA.Rnw",
                  "bancos/manual/desc_grupos_de_despesa.xlsx",
                  "bancos/manual/desc_fontes_de_recursos.xlsx",
