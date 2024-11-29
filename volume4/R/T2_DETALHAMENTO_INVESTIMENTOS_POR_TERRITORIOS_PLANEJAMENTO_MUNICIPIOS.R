813f8a5aab6dfa34b97a86c72ae42099cf057dc6
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-11-29 11:59:27 -0300

    Corrige script funcoes.r para R geracao vol4
    See #158
    See #160

--- volume4/R/T2_DETALHAMENTO_INVESTIMENTOS_POR_TERRITORIOS_PLANEJAMENTO_MUNICIPIOS.R
+++ volume4/R/T2_DETALHAMENTO_INVESTIMENTOS_POR_TERRITORIOS_PLANEJAMENTO_MUNICIPIOS.R
@@ -1,5 +1,5 @@
 # ===================================================================================================
-# Organização do banco DETALHAMENTO DOS INVESTIMENTOS POR TERRITÓRIOS DE PLANEJAMENTO E MUNICÍPIOS 
+# Organização do banco DETALHAMENTO DOS INVESTIMENTOS POR TERRITÓRIOS DE PLANEJAMENTO E MUNICÍPIOS
 #
 # Constroi bancos para cada UO. Apresenta os valores a nível de obra.
 # Para cada obra, destaca qual ação esta faz referência, bem como território e município realizado
@@ -7,7 +7,7 @@
 # ===================================================================================================
 
 options(warn = 1)
-source("utils/funcoes.r", encoding = "UTF-8")
+source("utils/funcoes.R", encoding = "UTF-8")
 source("utils/formataTexto.R", encoding = "UTF-8")
 
 # ====== LOAD das funções para abertura dos bancos necessários ===================
@@ -23,24 +23,24 @@ for(cod_uo in obras[, unique(UO)]){
   # cod_uo = 1301 # SETOP
   uo = obras[UO==cod_uo,]
   uo[, DESCRICAO_DA_OBRA := paste0(NUMERO_DA_OBRA_SISOR, " - ", DESCRICAO_DA_OBRA)]
-  
+
   # Especificação deve trazer o código até a ação e o descritivo da ação
   uo[, especificacao := paste0(formatC(FUNCAO,width = 2, flag="0"),"  " ,
                                formatC(SUBFUNCAO, width = 3, flag="0"), " ",
                                formatC(PROGRAMA, width = 3, flag="0"), " ",
-                               substr(ACAO,1,1), " ", 
+                               substr(ACAO,1,1), " ",
                                substr(ACAO, 2, 4))]
 
   uo[, cod_admin := SUBPROJETO]
-  
+
   # Descritivo da ação apresentado ao lado de especificação na tabela
   uo[, nome_acao := ""]
   for(acao in uo[, unique(ACAO)]){
     if(acao %in% acoes[, unique(cod_acao)]){
-      
+
       acao_titulo = acoes[cod_acao==acao, unique(titulo_acao)][1]
       uo[ACAO==acao, nome_acao := acao_titulo]
-      
+
       } else{
         warning(paste("T2_DETALHAMENTO_INVESTIMENTOS_POR_TERRITORIOS_PLANEJAMENTO_MUNICIPIOS: ",
                       "O codigo da Ação ", acao, " nao foi encontrado em SISOR/acoes_planejamento.xlsx"))
@@ -50,19 +50,19 @@ for(cod_uo in obras[, unique(UO)]){
   uo[, iniciando := ifelse(STATUS_DA_OBRA=="INICIANDO", 1, 0)]
   uo[, execucao := ifelse(STATUS_DA_OBRA=="EXECUÇÃO", 1, 0)]
   uo[, paralisada := ifelse(STATUS_DA_OBRA=="PARALISADO", 1, 0)]
-  
+
   # =====================================================================
-  
-  # Realiza a agregação dos VALOR_TESOURO e OUTROS pelo nível de detalhamento das características da 
+
+  # Realiza a agregação dos VALOR_TESOURO e OUTROS pelo nível de detalhamento das características da
   # obra (DESCRICAO_DA_OBRA, "status" etc)
   uo = uo[, list(VALOR_TESOURO = sum(VALOR_TESOURO, na.rm=T), VALOR_OUTROS = sum(VALOR_OUTROS, na.rm=T)),
-          by= list(REGIAO_GEOGRAFICA_INTERMEDIARIA, MUNICIPIO, DESCRICAO_DA_OBRA, QUANTIDADE, 
-                   UNIDADE_DE_MEDIDA_DA_OBRA, iniciando, execucao, 
+          by= list(REGIAO_GEOGRAFICA_INTERMEDIARIA, MUNICIPIO, DESCRICAO_DA_OBRA, QUANTIDADE,
+                   UNIDADE_DE_MEDIDA_DA_OBRA, iniciando, execucao,
                    paralisada, especificacao, cod_admin, nome_acao)]
-  
+
   # total agrega o valor por ação e subprojeto
-  total = uo[, list(VALOR_TESOURO = sum(VALOR_TESOURO, na.rm=T), 
-                    VALOR_OUTROS = sum(VALOR_OUTROS, na.rm=T)), 
+  total = uo[, list(VALOR_TESOURO = sum(VALOR_TESOURO, na.rm=T),
+                    VALOR_OUTROS = sum(VALOR_OUTROS, na.rm=T)),
              by=list(especificacao, cod_admin)]
 
   total[, cod_admin := cod_admin + 0.5] # Mantem a linha de total no final
@@ -70,26 +70,26 @@ for(cod_uo in obras[, unique(UO)]){
 
   uo = rbind(uo, total, fill=T)
   uo = uo[order(especificacao, cod_admin, REGIAO_GEOGRAFICA_INTERMEDIARIA, MUNICIPIO, DESCRICAO_DA_OBRA)]
-  
+
   # Insere nome de órgão e UO
   if(cod_uo %in% sumario[, COD_UO]){
     COD_ORGAO = sumario[COD_UO==cod_uo, unique1(COD_ORGAO)]
     ORGAO = sumario[COD_UO==cod_uo, unique1(ORGAO)]
     NOME_UO = sumario[COD_UO==cod_uo, unique1(UO)]
-    
-    uo[, orgao := paste0(substr(COD_ORGAO,1,1), ".", 
-                         substr(COD_ORGAO,2,3), ".", 
+
+    uo[, orgao := paste0(substr(COD_ORGAO,1,1), ".",
+                         substr(COD_ORGAO,2,3), ".",
                          substr(COD_ORGAO,4,4), " - ",  toupper(ORGAO))]
 
-    uo[, nome_uo := paste0(substr(cod_uo,1,1), ".", 
-                          substr(cod_uo,2,3), ".", 
+    uo[, nome_uo := paste0(substr(cod_uo,1,1), ".",
+                          substr(cod_uo,2,3), ".",
                           substr(cod_uo,4,4), " - ",  toupper(NOME_UO))]
   } else{
     uo[, c("orgao", "nome_uo") := NA]
     warning(paste("T2_DETALHAMENTO_INVESTIMENTOS_POR_TERRITORIOS_PLANEJAMENTO_MUNICIPIOS: O codigo UO ",
                   cod_uo, " não achou correspondência no sumário. Alterar em volume4/data/sumario_v4.txt"))
   }
-  
+
   colsDelete = which(names(uo) %in% c("nome_uo", "orgao", "cod_admin", "nome_acao"))
 
   # Quero tornar NA todas as linhas que não sejam as linhas logo após a DESCRIÇÃO DA OBRA igual a TOTAL.
@@ -99,7 +99,7 @@ for(cod_uo in obras[, unique(UO)]){
   uo = uo[setdiff(1:nrow(uo), append(c(1) ,(which(DESCRICAO_DA_OBRA=="TOTAL") + 1))), (colsDelete):=NA]
 
   uo = uo[,lapply(.SD, formatarNum)]
-  
+
   uo[, DESCRICAO_DA_OBRA := correcaoCaracteresEspeciais(DESCRICAO_DA_OBRA, caracteres)]
   uo[, REGIAO_GEOGRAFICA_INTERMEDIARIA := correcaoCaracteresEspeciais(REGIAO_GEOGRAFICA_INTERMEDIARIA, caracteres)]
   uo[, MUNICIPIO := correcaoCaracteresEspeciais(MUNICIPIO, caracteres)]
@@ -107,14 +107,14 @@ for(cod_uo in obras[, unique(UO)]){
   uo[, nome_acao := correcaoCaracteresEspeciais(nome_acao, caracteres)]
   uo[, orgao := correcaoCaracteresEspeciais(orgao, caracteres)]
   uo[, nome_uo := correcaoCaracteresEspeciais(nome_uo, caracteres)]
-  
-  
-  setnames(uo, c("REGIAO_GEOGRAFICA_INTERMEDIARIA", "MUNICIPIO", "DESCRICAO_DA_OBRA", "UNIDADE_DE_MEDIDA_DA_OBRA", 
+
+
+  setnames(uo, c("REGIAO_GEOGRAFICA_INTERMEDIARIA", "MUNICIPIO", "DESCRICAO_DA_OBRA", "UNIDADE_DE_MEDIDA_DA_OBRA",
                  "QUANTIDADE", "VALOR_TESOURO", "VALOR_OUTROS"),
-               c("territorio", "municipio", "obra", "unidade", 
+               c("territorio", "municipio", "obra", "unidade",
                  "qtde", "tesouro", "outros"))
 
-  write.table(uo, paste0("volume4/data/tabela2/", cod_uo,".txt"), append = FALSE, 
+  write.table(uo, paste0("volume4/data/tabela2/", cod_uo,".txt"), append = FALSE,
               quote = FALSE, sep = "\t", na = "", dec = ",", row.names = FALSE)
 
 }
--- volume4/R/sumario_v4.R
+++ volume4/R/sumario_v4.R
@@ -1,11 +1,11 @@
 # ==========================================================================
-# Utiliza os nomes de ORGAO, UO e PODER para presentes em QDD_FISCAL e 
-# QDD_INVESTIMENTO para montar o sumário. Este manterá apenas os nomes das 
+# Utiliza os nomes de ORGAO, UO e PODER para presentes em QDD_FISCAL e
+# QDD_INVESTIMENTO para montar o sumário. Este manterá apenas os nomes das
 # UO's que aparecem no banco BASE_DETALHAMENTO_OBRAS.xlsx
 # ==========================================================================
 
 options(warn = 1)
-source("utils/funcoes.r", encoding = "UTF-8")
+source("utils/funcoes.R", encoding = "UTF-8")
 source("utils/formataTexto.R", encoding = "UTF-8")
 
 # ====== LOAD das funções para abertura dos bancos necessários ===================
@@ -33,15 +33,15 @@ sumario = sumario[order(PODER, COD_ORGAO, COD_UO)]
 obras = trataDetalhe_Obras("bancos/SISOR/BASE_DETALHAMENTO_OBRAS.xlsx", acoes="Não utilizar", realizarTeste = F)
 
 if(length(setdiff(obras[, unique(UO)], sumario[, unique(COD_UO)]))>0){
-  
+
   warning(paste("Há códigos de UO que estão presentes no banco BASE_DETALHAMENTO_OBRAS.xlsx",
                 "mas não estão presentes nos bancos do volume 3 BASE_QDD_INVESTIMENTO.xlsx",
-                "e do volume 5 BASE_QDD_FISCAL. Verificar as UO's ", 
+                "e do volume 5 BASE_QDD_FISCAL. Verificar as UO's ",
                 setdiff(obras[, unique(UO)], sumario[, unique(COD_UO)]),
                 "e realizar a inclusão dessas UO's em volume4/data/sumario.txt"))
 }
 
 sumario = sumario[COD_UO %in% unique(obras$UO),]
 
-write.table(sumario[,merge:=NULL], "volume4/data/sumario_v4.txt", append = FALSE, quote = T, 
+write.table(sumario[,merge:=NULL], "volume4/data/sumario_v4.txt", append = FALSE, quote = T,
             sep = "\t",  na = "", dec = "@", row.names = FALSE)
