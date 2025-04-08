90614ca69b1187cbdef9bc08ee4733f6b3fe4b7c
labanca <moraislabanca@gmail.com>
2025-04-08 16:48:30 -0300

    fix(dados-t17-t18): atualiza dados para corrigir erros no valor do FES. see #171

--- volume1/R/T18_DCGF_Demonst_Aplicacao_Recursos_Acoes_Servicos_Publicos_Saude.R
+++ volume1/R/T18_DCGF_Demonst_Aplicacao_Recursos_Acoes_Servicos_Publicos_Saude.R
@@ -62,7 +62,6 @@ parteB_desc =  "B. DESPESA COM SAÚDE"
 
 loa_desp = loa_desp[is_asps_desp(loa_desp, "ELEMENTO_ITEM_COD"),]
 
-loa_desp = loa_desp[!(MODALIDADE_COD==91 & ELEMENTO_COD == 41)]
 
 parteB = loa_desp[, list(VL_LOA = sum(VL_DESP), nvl=1), 
                   by= list(cod = paste0(UO_COD,".", FUNCAO_COD), espec = UO)][order(cod)]
