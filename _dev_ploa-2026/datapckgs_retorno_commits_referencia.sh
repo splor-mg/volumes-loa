06f9d9a7df496f23de0649a882abc24607647c1d
carloshob <carloshenrique.ob@gmail.com>
2025-09-25 15:08:00 -0300

    refac(dev): altera nome para maior clareza
    See https://github.com/splor-mg/volumes-loa/issues/175

--- _dev_ploa-2026/datapckgs_retorno_commits_referencia.sh
+++ _dev_ploa-2026/datapckgs_retorno_commits_referencia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # =============================================================================
-# Script para reverter datapackages conforme _temp/datapckgs_commits_ultima_ploa.yml
+# Script para reverter datapackages conforme _temp/datapckgs_commits_referencia.yml
 # =============================================================================
 
 set -e  # Para o script em caso de erro
@@ -57,7 +57,7 @@ print_info "=== REVERTENDO DATAPACKAGES PARA VERSÕES LOA 2025 ==="
 echo
 
 # Ler commits do YAML
-COMMITS_FILE="_temp/datapckgs_commits_ultima_ploa.yml"
+COMMITS_FILE="_temp/datapckgs_commits_referencia.yml"
 [ -n "$1" ] && COMMITS_FILE="$1"
 if [ ! -f "$COMMITS_FILE" ]; then
     print_error "Arquivo de commits não encontrado: $COMMITS_FILE"
