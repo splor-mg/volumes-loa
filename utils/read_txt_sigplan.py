915e969c91718bc64760b44d1f1663c1932c9297
Maria Duque <maria.oliveira.duque@gmail.com>
2026-08-28 08:12:15 -0300

    altera path e encoding da leitura de 'acoes_planejamento'

--- utils/read_txt_sigplan.py
+++ utils/read_txt_sigplan.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-filename = Path('datapackages/sigplan/data-raw/acoes_planejamento.txt')
+filename = Path('datapackages/sigplan/data/acoes_planejamento.txt')
 save_path = Path('bancos/SISOR/acoes_planejamento.txt')
 
-with open(filename, 'r', encoding='latin1') as source:
+with open(filename, 'r', encoding='utf-8') as source:
   content = source.read()
 
 with open(save_path, 'w', encoding='utf-8') as destination:
